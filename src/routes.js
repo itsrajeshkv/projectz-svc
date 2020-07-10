55b92ca67e1410691b49eb62c0eef84d063a406d
root <root@myLinuxVM.uhpecnyzuooefglpo2dulngtuf.ax.internal.cloudapp.net>
2020-07-10 11:45:17 +0000

    add hello - world route

--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,16 @@ const appRouter = express.Router()
 const projects = require( './data' )
 
 appRouter.get( '/services/projects', function( req, res ) {
-  res.json({ code: 'success', payload: projects })
+  res.json({ code: 'successful', payload: projects })
 })
 
 appRouter.get( '/services/projects/:id', function( req, res ) {
   const project = projects.find( p => p.projectId === parseInt( req.params.id ) )
-  res.json({ code: 'success', payload: project })
+  res.json({ code: 'successful', payload: project })
 })
 
 appRouter.get( '/services/hello', function( req, res ) {
-  res.json({ code: 'success', payload: 'World' })
+  res.json({ code: 'successful', payload: 'World' })
 })
 
 module.exports = appRouter
